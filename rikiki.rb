4c0ab29754c9ede25b7c82b4b2b7832e253bcaca
yann ARMAND <yann@harakys.com>
2012-03-29 17:18:45 +0200

    filename from path

--- rikiki.rb
+++ rikiki.rb
@@ -6,9 +6,16 @@ get '/' do
 end
 
 get %r{/wiki(/.*)} do |path|
+  filename = get_filename(path)
 end
 
 post %r{/wiki(/.*)} do |path|
+  filename = get_filename(path)
 end
 
+def get_filename(path)
+  path += 'index' if path =~ /\/$/
+  filename = "pages#{path}.md"
+  filename
+end
 
